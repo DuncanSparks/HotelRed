30f196c3724a8724bdbb72528fcceeac14d48f78
Duncan Sparks <duncan.sprks@gmail.com>
2019-04-18 17:17:18 -0700

    Fix items

--- Scripts/System/Item.cs
+++ Scripts/System/Item.cs
@@ -1,31 +1,22 @@
 using Godot;
 using System;
 
-public class Item : Control
+public class Item : TextureButton
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // refs
     [Export]
-    private Sprite image = null;
+    private Texture itemTexture;
+
     [Export]
     private string itemName = "Item Name";
+
     [Export]
     private string itemDescription = "Item Description";
-    // Called when the node enters the scene tree for the first time.
-    public Sprite Image { get { return image; } set { image = value; } }
+
     public string ItemName { get { return itemName; } set { itemName = value; } }
     public string ItemDescription { get { return itemDescription; } set { itemDescription = value; } }
+
     public override void _Ready()
     {
-        GetNode<Sprite>("Image").SetTexture(image.Texture);
+		GetNode<TextureRect>("TextureRect").Texture = itemTexture;
     }
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
