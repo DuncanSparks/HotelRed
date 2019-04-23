2e5c63e3b9d76773fae6f6396e9310e2cceb793f
Duncan Sparks <duncan.sprks@gmail.com>
2019-04-23 15:42:35 -0700

    Fix UI problem

--- Scripts/Globals/Player.cs
+++ Scripts/Globals/Player.cs
@@ -78,9 +78,6 @@ public class Player : KinematicBody2D
 
 	private int numItems = 0;
 
-
-
-
 	// ================================================================
 
 	public static ST State { get { return Player.Main.state; } set { Player.Main.state = value; } }
@@ -95,6 +92,7 @@ public class Player : KinematicBody2D
 	public static int NumItems { get { return Player.Main.numItems; } set { Player.Main.numItems = value; } }
 
 	public static bool InventoryLock { get { return Player.Main.inventoryLock; } set { Player.Main.inventoryLock = value; } }
+	
 	// ================================================================
 
 	// Inventory
@@ -189,11 +187,14 @@ public class Player : KinematicBody2D
 		Player.MotionOverrideVec = new Vector2(0, 0);
 	}
 
+
 	public static void FreePlayer()
 	{
 		Player.State = Player.ST.MOVE;
 		Player.MotionOverride = false;
 	}
+
+
 	public static Camera2D GetCamera()
 	{
 		return Player.Main.GetNode<Camera2D>("Camera");
@@ -266,8 +267,4 @@ public class Player : KinematicBody2D
 				return walking ? spriteSetNormalWalk[(int)direction] : spriteSetNormal[(int)direction];
 		}
 	}
-
-    private class RoomKey
-    {
-    }
 }
