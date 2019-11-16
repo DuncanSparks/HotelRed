2c6f75ff5a5dd79bb3e44d28fe0f090912ddace2
Duncan Sparks <duncan.sprks@gmail.com>
2019-11-15 17:45:54 -0800

    Fix shit

--- Scripts/Scenes/RecRoom.cs
+++ Scripts/Scenes/RecRoom.cs
@@ -6,9 +6,15 @@ public class RecRoom : Node2D
 	[Export]
 	private NodePath npc;
 
+	[Export]
+	private NodePath paper;
+
     public override void _Ready()
     {
-        if (Controller.Flag("item_paper") == 1)
+        if (Controller.Flag("item_paper") == 1 || Controller.Flag("neftali_cutscene") == 0)
+		{
 			GetNode<EventNPC>(npc).QueueFree();
+			GetNode<WorldItem>(paper).QueueFree();
+		}	
     }
 }
